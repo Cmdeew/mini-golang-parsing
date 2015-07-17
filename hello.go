f6c7c7f947615f8655be5086fffd36deb52a81dc
Thomas Rieux-Laucat <rieux@neptentia.lan>
2015-07-17 02:36:24 +0200

    part 1: parsing

---
+++ hello.go
@@ -0,0 +1,59 @@
+package hello
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+	"appengine"
+	"appengine/urlfetch"
+
+)
+
+type cont 		struct {
+	Page 		int `json:"page"`
+	Limit 		int `json:"limit"`
+	Explicit 	bool `json:"explicit"`
+	Total 		int `json:"total"`
+	Has_more 	bool `json:"has_more"`
+	List 		[]Video `json:"list"`
+}
+
+type Video 	struct {
+	Id 		string `json:"id"`
+	Title 	string `json:"title"`
+	Channel string `json:"channel"`
+	Owner 	string `json:"owner"`
+}
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World ! Let's start\n\n")
+
+	url := "https://api.dailymotion.com/channel/music/videos"
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+	res, err := client.Get(url)
+	if err != nil{
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Fprint(w, string(body))
+	if err != nil{
+		panic(err.Error())
+	}
+	//Decode
+	var data cont
+	err = json.Unmarshal([]byte(body), &data)
+	if err != nil{
+		panic(err.Error())
+	}
+
+	fmt.Fprint(w, "\n!!!!!!!!!! DEBUG !!!!!!!!!!!!!!!!\n")
+	for i, track := range data.List{
+		fmt.Fprint(w, track.Title + "-", i)
+	}
+}
